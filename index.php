4fa7aa79da13a82b0751b50753ae10a84b29d893
Andrew <code70@inbox.ru>
2023-05-22 15:20:36 +0700

    loader and font fix

--- index.php
+++ index.php
@@ -1,12 +1,32 @@
 <!DOCTYPE html>
-<html>
+<html lang="RU-ru">
     <head>
         <meta name="robots" content="noindex, nofolow">
-        <title>title</title>
+        <title>Каникулы в Грядка Сити</title>
         <meta name="description" content="description">
+        <meta name="author" content="Andrew Korobkov (http://code-me.ru)">
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+        <meta name="keywords" content="">
+        <link rel="stylesheet" href="assets/css/bootstrap.min.css">
         <link rel="stylesheet" href="assets/css/style.css">
     </head>
-    <body></body>
-        <h1>asdasdf</h1>
+    <body>        
+    <div class="loader">
+        <div class="loader-anim-logo">
+            <object data="assets/images/svg/logo.svg" type=""></object>
+            <h3>грядка сити</h3>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h2>asdsads</h2>
+            </div>
+        </div>
+    </div>
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/js/bootstrap.bundle.js"></script>
+    <script src="assets/js/app.js"></script>
     </body>
 </html>
