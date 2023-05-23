294ca67992a12ff404ea941c70ffb4acc3392aab
Andrew <code70@inbox.ru>
2023-05-23 14:55:55 +0700

    front template

--- index.php
+++ index.php
@@ -12,19 +12,117 @@
         <link rel="stylesheet" href="assets/css/style.css">
     </head>
     <body>        
-    <div class="loader">
-        <div class="loader-anim-logo">
-            <object data="assets/images/svg/logo.svg" type=""></object>
-            <h3>грядка сити</h3>
-        </div>
-    </div>
-    <div class="container">
-        <div class="row">
-            <div class="col-12">
-                <h2>asdsads</h2>
+    <div class="hero-wrapper">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <h1>Каникулы в Грядка Сити</h1>
+                    <h2>10 дневынй курс социального проектирования</h2>
+                </div>
             </div>
         </div>
     </div>
+
+    <section>
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <h2>Для кого подойдет этот курс</h2>
+                    <ul>
+                        <li>Для ребят, которые генерируют и хотят представить свои идеи</li>
+                        <li>Для ребят, которым нужно находится в сильном окружении</li>
+                        <li>Для ребят, которые хотят прокачать навыки предпринимательства</li>
+                        <li>Для ребят, которые творчески и креативны</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section>
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <h2>Как будет проходить</h2>
+                    <ul>
+                        <li>Группа edutaiment проходит отдельно от игровой программы МК, тренинги и задания наставника</li>
+                        <li>На 5 день дети будут знакомиться с направлениями в котором могут создать проект: помощь животным, людям, планете или соц. бизнес</li>
+                        <li>Каждый ребенок определит, хочет он в команде с кем-то делать проект или самостоятельно</li>
+                        <li>Наставник будет поддерживать связь с ребятами, чтобы подготовить проект</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section>
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <h2>Что получит ребенок</h2>
+                    <ul>
+                        <li>Каждый ребенок получит набор навыков: креативное мышление, критическое мышление, работа в команде, публичные выступления</li>
+                        <li>Готовый проект для выступления на конференции Грядка Сити в августе</li>
+                        <li>На конференции каждый ребенок получит призы, а также будут определены победиттели по направлениям</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section>
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <h2>Стоимость</h2>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12 col-md-6 col-lg-6">
+                    <h3>10 дней каникул</h3>
+                    <ul>
+                        <li>Пребывание на площадке с 9:00 до 17:00 10 дней</li>
+                        <li>Трехразовое питание + перекус</li>
+                        <li>Каждый день мастер-класс или выезд: кино, лабиринт, МК по пицце</li>
+                        <li>Каждый день - интелектуальное, спортивное или развлекательное мероприятие</li>
+                        <li>Прогулки на свежем воздухе</li>
+                    </ul>  
+                </div>
+                <div class="col-12 col-md-6 col-lg-6">
+                    <h3>5 дней каникул</h3>
+                    <ul>
+                        <li>Пребывание на площадке с 9:00 до 17:00 5 дней</li>
+                        <li>Трехразовое питание + перекус</li>
+                        <li>Каждый день мастер-класс или выезд: кино, лабиринт, МК по пицце</li>
+                        <li>Каждый день - интелектуальное, спортивное или развлекательное мероприятие</li>
+                        <li>Прогулки на свежем воздухе</li>
+                    </ul>  
+                </div>
+            </div>
+        </div>
+    </section>
+    <section>
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <h2>Контакты</h2>
+                </div>
+            </div>
+            <div class="row d-flex align-items-center">
+                <div class="col-12 col-md-6 col-lg-6">
+                    <h3>Контакты</h3>
+                    <ul>
+                        <li>Адрес: Б. Подгорная 87</li>
+                        <li>Телефон: +7(999)999-99-99</li>
+                        <li>Мессенждеры: TG/WA/VK/INST</li>
+                    </ul>
+                </div>
+                <div class="col-12 col-md-6 col-lg-6">
+                    <div class="map-wrapper">
+                        <img src="http://via.placeholder.com/1024x512" width="100%" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
     <script src="assets/js/jquery.min.js"></script>
     <script src="assets/js/bootstrap.bundle.js"></script>
     <script src="assets/js/app.js"></script>
